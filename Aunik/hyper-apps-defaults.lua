31688e7d39393b6cc72fe2410b33cc11f237a247
Mehechiger <Mehechiger@users.noreply.github.com>
2020-06-08 23:04:32 +0200

    alpha1 setup

---
+++ Aunik/hyper-apps-defaults.lua
@@ -0,0 +1,15 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'b', 'Google Chrome', 'tell application "/Applications/Google Chrome.app"\n  make new window\n  activate\nend tell' },     -- "B" for "Browser"
+  { 'f', 'Finder', 'tell application "Finder" to make new Finder window' },            -- "F" for "Finder"
+  { 't', 'iTerm', 'tell application "iTerm2"\n  create window with default profile\nend tell' },             -- "T" for "Terminal"
+}
+--  { 'a', 'Music' },             -- "A" for "Apple Music"
+--  { 'c', 'Slack' },             -- "C for "Chat"
+--  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
+--  { 'e', 'Atom' },              -- "E" for "Editor"
+--  { 'g', 'Mailplane 3' },       -- "G" for "Gmail"
---
+++ Aunik/usb_watcher.lua
@@ -0,0 +1,9 @@
+function usbDeviceCallback(data)
+  print("usbDeviceCallback: "..hs.inspect(data))
+end
+
+
+
+usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
+usbWatcher:start()
+
